446bc61a846130002b9d124862d2920ad5f09dc5
pete3249 <katelynrpeterson16@gmail.com>
2020-12-15 12:15:24 -0500

    commit tree e30a328af01155c641111eec225ddcc13ade06d5

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let result = record.find(game => game["result"] === "W")
+  return !!result ? result["year"] : undefined
+}
